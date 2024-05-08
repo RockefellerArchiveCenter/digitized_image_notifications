5f3b871ad3da5870053e58ac12b9fa4f3a119c35
Hillel Arnold <hillel.arnold@gmail.com>
2024-05-08 12:32:28 -0400

    use correct url

--- src/handle_digitized_image_notifications.py
+++ src/handle_digitized_image_notifications.py
@@ -77,8 +77,8 @@ def structure_teams_message(color_name, title, message, traceback, facts):
         ]
     }
     if facts['Outcome'] == 'started':
-        url = 'https://digitzed-image-qc.dev.rockarch.org' if environ.get(
-            'ENV') == 'dev' else 'https://digitzed-image-qc.rockarch.org'
+        url = 'https://digitized-image-qc.dev.rockarch.org' if environ.get(
+            'ENV') == 'dev' else 'https://digitized-image-qc.rockarch.org'
         notification['attachments'][0]['content']['actions'] = [
             {
                 "type": "Action.OpenUrl",
